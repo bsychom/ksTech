28229cd993b675b83fd7d3646c40557243bee790
mhaddaou <medo19981602@gmail.com>
2024-06-06 13:01:07 +0100

    up

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center relative bg-[url('/background.svg')] bg-no-repeat bg-cover bg-center">
+      {/* <Image src="background.svg" alt="45" className="w-full " height={0} width={0} /> */}
+   
+
+    </div>
+    
+  );
+}
