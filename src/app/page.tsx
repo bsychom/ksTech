c0f7dd97147696038755d668a9cc766f0bbc03c8
mhaddaou <medo19981602@gmail.com>
2024-06-11 20:14:08 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,13 @@ export default function Home() {
         <Header/>
         <Hero/>
         {/* bg-[#001F3F]/20 */}
-        <div className="pt-10 w-full relative">
+        <div className="pt-10 w-full relative container mx-auto">
           <div className="absolute -left-8 w-[200px] h-[200px]  bg-[#001F3F]/80 blur-3xl">
             lskdjf
           </div>
           <div className="w-full ">
             <p className="text-center text-2xl font-bold tracking-wide underline pb-10">Avantages du câblage structuré</p>
-            <p className="w-[81%] mx-auto font-medium tracking-wide inter.className">Bien que les câbles et les fils soient souvent cachés derrière les murs, les solutions de câblage structuré peuvent offrir aux entreprises et aux centres de données des avantages indispensables. Sans cela, le système est en danger, notamment lors de la maintenance. Certains avantages du câblage structuré incluent :</p>
+            <p className=" w-[85%] mx-auto text-center font-medium tracking-wide inter.className">Bien que les câbles et les fils soient souvent cachés derrière les murs, les solutions de câblage structuré peuvent offrir aux entreprises et aux centres de données des avantages indispensables. Sans cela, le système est en danger, notamment lors de la maintenance. Certains avantages du câblage structuré incluent :</p>
           </div>
         </div>
         
